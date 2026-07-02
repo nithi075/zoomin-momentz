4229d3dd3c50ab8b663182aa5ae6f07906f227ac
Sam Nithish <nithishraj8055@gmail.com>
2026-07-02 14:55:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Gallery from './components/Gallery/Gallery'
-import Testimonials from './components/Testimonials/Testimonials'
+import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
-import Instagram from './components/instagram/instagram'
+import Instagram from './components/instagram/Instagram'
 import FAQ from './components/FreequentQ/faq'
-import Footer from './components/Footer/Footer'
+import Footer from './components/footer/footer'
 import Portfolio from './components/Portfolio/Portfolio'
 import ScrollToTop from './components/ScrollToTop'
 
 import './App.css'
-import Floating from './components/Floating/Floating'
+import Floating from './components/floating/floating'
 
 function Home() {
   return (
